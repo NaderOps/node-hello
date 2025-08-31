da2b70f98bf5cf6897a372b2cf4231b96859946f
Nader Tarek <nader.tarek@thndr.app>
2025-08-31 21:30:26 +0300

    terraform to deploy the app and the new relic infrastructure agent to forward logs

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+# ----------------------------
+# Hello-node image from GHCR
+# ----------------------------
+resource "docker_image" "hello_node" {
+  name = "ghcr.io/naderops/node-hello:master"
+}
+
+resource "docker_container" "hello_node" {
+  name  = "hello-node"
+  image = docker_image.hello_node.name
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+
+  restart = "always"
+}
+
+# ----------------------------
+# New Relic log forwarder
+# ----------------------------
+resource "docker_image" "newrelic_infra_agent" {
+  name = "newrelic/infrastructure:latest"
+}
+
+resource "docker_container" "newrelic_infra_agent" {
+  name  = "fluentbit-service"
+  image = docker_image.fluent_bit.name
+  privileged = true
+
+  env = [
+    "NRIA_LICENSE_KEY=${var.newrelic_license_key}",
+    "NRIA_LOGGING_ENABLED=true",
+    # The following are optional but recommended for better metadata in New Relic
+    "NRIA_DISPLAY_NAME=docker-host",
+    "NRIA_CUSTOM_ATTRIBUTES={\"environment\":\"development\"}",
+  ]
+
+  # Mount necessary host directories for the agent to function
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+    read_only      = false
+  }
+  volumes {
+    host_path      = "/"
+    container_path = "/host"
+    read_only      = true
+  }
+
+  # Restart policy in case the container stops
+  restart = "unless-stopped"
+
+}
